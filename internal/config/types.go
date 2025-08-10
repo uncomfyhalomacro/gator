e12e8f2436772c0075523e74ed963ed2a6b264c5
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-08-10 11:24:54 +0800

    change field type to string
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- internal/config/types.go
+++ internal/config/types.go
@@ -2,5 +2,5 @@ package config
 
 type Config struct {
 	DbUrl           string `json:"db_url"`
-	CurrentUsername any    `json:"current_user_name"`
+	CurrentUsername string `json:"current_user_name"`
 }
